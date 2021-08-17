40274d9abce103d58ebf0954feee2883dd3a66c0
Corentin BOGAERT <bogaert.corentin@hotmail.fr>
2021-08-17 15:59:16 +0200

    Push 1

---
+++ ENIProjet/src/fr/eni/enchere/bo/ArticleVendu.java
@@ -0,0 +1,84 @@
+package fr.eni.enchere.bo;
+
+import java.time.LocalDate;
+
+public class ArticleVendu {
+
+	int  noArticle;
+	String nomArticle;
+	String description;
+	LocalDate dateDebutEncheres;
+	LocalDate dateFinEncheres;
+	int miseAPrix;
+	int prixVente;
+	String etatVente;
+	public int getNoArticle() {
+		return noArticle;
+	}
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+	public String getNomArticle() {
+		return nomArticle;
+	}
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public LocalDate getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+	public void setDateDebutEncheres(LocalDate dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+	public LocalDate getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+	public void setDateFinEncheres(LocalDate dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+	public int getMiseAPrix() {
+		return miseAPrix;
+	}
+	public void setMiseAPrix(int miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+	public int getPrixVente() {
+		return prixVente;
+	}
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+	public String getEtatVente() {
+		return etatVente;
+	}
+	public void setEtatVente(String etatVente) {
+		this.etatVente = etatVente;
+	}
+	public ArticleVendu(int noArticle, String nomArticle, String description, LocalDate dateDebutEncheres,
+			LocalDate dateFinEncheres, int miseAPrix, int prixVente, String etatVente) {
+		super();
+		this.noArticle = noArticle;
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+	}
+	@Override
+	public String toString() {
+		return "ArticleVendu [noArticle=" + noArticle + ", nomArticle=" + nomArticle + ", description=" + description
+				+ ", dateDebutEncheres=" + dateDebutEncheres + ", dateFinEncheres=" + dateFinEncheres + ", miseAPrix="
+				+ miseAPrix + ", prixVente=" + prixVente + ", etatVente=" + etatVente + "]";
+	}
+	
+	
+	
+}
